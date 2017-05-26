f716a4f23a0c2b5c90871295c741bf396b17cbe1
Dervis <dervisgomez77@gmail.com>
2017-05-26 15:01:27 -0400

    Genesis Listo

---
+++ src/json/Main.java
@@ -0,0 +1,128 @@
+package json;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+
+public class Main {
+	
+	static int xxx=0;
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text=cargarLista();
+		String[] verso=text.split("00");
+		generarJSON(verso);
+		for (int x=0;x<verso.length;x++){
+			System.out.println("-VERSO: "+verso[x]+"\n");
+		}
+		
+
+	}
+	
+	public static void generarJSON(String[] verso){
+		try {
+			JsonObject o = new JsonObject();
+			JsonArray a=new JsonArray();
+			for(int x=0;x<verso.length;x++){
+				JsonObject jsonObject2 = new JsonObject();
+				jsonObject2.addProperty("numero", x+1);
+				jsonObject2.addProperty("texto", verso[x]);
+				a.add(jsonObject2);
+			}
+			o.add("versiculos", a);
+			o.addProperty("numero", xxx);
+			
+			JsonObject o2 = new JsonObject();
+			JsonArray a2=new JsonArray();
+			a2.add(o);
+			o2.add("capitulos",a2);
+			o2.addProperty("libro","Génesis");
+			JsonArray a3=new JsonArray();
+			a3.add(o2);
+			System.out.println(o.toString());
+			guardarArchivo(o.toString());
+                
+        } catch (JsonIOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public static void guardarArchivo(String texto){
+		
+		Path path=Paths.get("prueba.txt");
+		Charset utf8 = StandardCharsets.UTF_8;
+		String tmp;
+		String text="";
+		try(BufferedReader r =Files.newBufferedReader(path,utf8)){
+			while((tmp = r.readLine()) != null){
+				text=text+tmp+"\n";				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		Path path2=Paths.get("prueba.txt");
+		Charset utf82 = StandardCharsets.UTF_8;
+		
+		try(BufferedWriter w =Files.newBufferedWriter(path2, utf82)){
+			w.write(text+","+texto);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static String cargarLista(){
+		Path path=Paths.get("archivo.txt");
+		Charset utf8 = StandardCharsets.UTF_8;
+		String tmp;
+		String text="";
+		try(BufferedReader r =Files.newBufferedReader(path,utf8)){
+			while((tmp = r.readLine()) != null){
+				
+						String[] ll=tmp.split(" ");
+						for(int x=0;x<ll.length;x++){
+							if(!ll[x].equals("")){
+								if(isNumeric(ll[x])){
+									if(text.equals("")){
+										text=text+"1"+" ";
+										xxx=Integer.valueOf(ll[x]);
+									}else{
+										text=text+"00"+ll[x]+" ";										
+									}
+								}else{
+									text=text+ll[x]+" ";
+								}
+							}
+						}					
+				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return text;
+	}
+	private static boolean isNumeric(String cadena){
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe){
+			return false;
+		}
+	}
+
+}
